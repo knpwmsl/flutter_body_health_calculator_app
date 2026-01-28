f604343b50ee151db28163b2f216783bc36d8bf3
knpwmsl <s6619410008@sau.ac.th>
2026-01-28 12:31:25 +0700

    Commit 02: NavigationBar เสร็จ

---
+++ lib/views/home_ui.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_body_health_calculator_app/views/about_ui.dart';
+import 'package:flutter_body_health_calculator_app/views/bmi_ui.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeUi extends StatefulWidget {
+  const HomeUi({super.key});
+
+  @override
+  State<HomeUi> createState() => _HomeUiState();
+}
+
+class _HomeUiState extends State<HomeUi> {
+  int currentIndexStatus = 1;
+  List<Widget> showbody = [
+    BmiUi(),
+    AboutUi(),
+    BmiUi(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF5AC481),
+        title: Text(
+          'body Health Calculator',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            currentIndexStatus = value;
+          });
+        },
+        currentIndex: currentIndexStatus,
+        selectedItemColor: Color(0xFF5AC481),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_2_outlined),
+            label: 'BMI',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'About',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              FontAwesomeIcons.personBooth,),
+            label: 'BMR',
+          ),
+        ],
+      ),
+      body: showbody[currentIndexStatus],
+    );
+  }
+}
